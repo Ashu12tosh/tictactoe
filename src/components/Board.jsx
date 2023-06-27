d106fcd5b1d3f81adad0552e1eef9410655880e9
Ashu12tosh <ash8707672263@gmail.com>
2023-06-27 09:53:15 +0530

    Designing Board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,34 @@
 import Square from './Square';
 
-const Board = () => {
+const Board = ({ squares, handleSquareClick, winningSquares }) => {
+  const renderSquare = position => {
+    const isWinningSquare = winningSquares.includes(position);
+
+    return (
+      <Square
+        value={squares[position]}
+        onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square val={0} />
-        <Square val={1} />
-        <Square val={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square val={3} />
-        <Square val={4} />
-        <Square val={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square val={6} />
-        <Square val={7} />
-        <Square val={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,14 @@
-const Square = ({ val }) => {
+const Square = ({ value, onClick, isWinningSquare }) => {
+  const colorClassName = value === 'X' ? 'text-green' : 'text-orange';
+  const winningClassName = isWinningSquare ? 'winning' : '';
+
   return (
-    <button type="button" className="Square">
-      {val}
+    <button
+      type="button"
+      className={`square ${colorClassName} ${winningClassName}`}
+      onClick={onClick}
+    >
+      {value}
     </button>
   );
 };
